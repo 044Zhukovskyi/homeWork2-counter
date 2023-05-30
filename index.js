54a448d9c638f11f75f60445c2be407fd5a24d26
Volodymyr Zhukoskyi <044.zhukovskyi.volodymyr@gmail.com>
2023-05-30 17:58:16 +0300

    homeWork2-counter

---
+++ index.js
@@ -0,0 +1,18 @@
+const hours = prompt('How many hours do you want to convert to seconds?')?.trim();
+const seconds = 3600;
+
+  if ( isNaN(hours) || !hours ) {
+    alert('Error! Incorrect input value!');
+  } else if(hours <= 0) {
+    alert('Error! Input value is negative or 0!');
+  }
+  else {
+    result = hours * seconds;
+    alert('Total: ' + result);
+  }
+
+
+
+
+  
+ 
